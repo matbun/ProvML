ff6b8f5ab4fa39b0005f3d33660c1316c3f09839
Matteo Bunino <matteo.bunino@gmail.com>
2024-06-14 16:45:10 +0200

    UPDATE setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='prov4ml',
     version='1.0.0',
     packages=find_packages(),
-    install_requires=[],  # List any dependencies your package requires
+    install_requires=required,  # Loaded from requirements.txt
 )
