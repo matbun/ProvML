small fix and added energy consumption calculation

prov4ml/logging.py
@@ -130,7 +130,6 @@ def log_model(model: Union[torch.nn.Module, Any], model_name: str = "default", l
     if log_as_artifact:
         save_model_version(model, model_name, Context.EVALUATION)
         
-
 def log_flops_per_epoch(label: str, model: Any, dataset: Any, context: Context, step: Optional[int] = None) -> None:
     """Logs the number of FLOPs (floating point operations) per epoch for the given model and dataset.
     
@@ -172,8 +171,8 @@ def log_system_metrics(
     log_metric("disk_usage", system_utils.get_disk_usage(), context, step=step, source=LoggingItemKind.SYSTEM_METRIC)
     log_metric("gpu_memory_usage", system_utils.get_gpu_memory_usage(), context, step=step, source=LoggingItemKind.SYSTEM_METRIC)
     log_metric("gpu_usage", system_utils.get_gpu_usage(), context, step=step, source=LoggingItemKind.SYSTEM_METRIC)
-    log_metric("gpu_power_usage", system_utils.get_gpu_power_usage(), context, step=step, source=LoggingItemKind.SYSTEM_METRIC)
     log_metric("gpu_temperature", system_utils.get_gpu_temperature(), context, step=step, source=LoggingItemKind.SYSTEM_METRIC)
+    log_metric("gpu_power_usage", system_utils.get_gpu_power_usage(), context, step=step, source=LoggingItemKind.SYSTEM_METRIC)
 
 def log_carbon_metrics(
     context: Context,

prov4ml_torch.py
@@ -23,7 +23,7 @@ prov4ml.start_run(
 
 prov4ml.register_final_metric("MSE_test", 10, prov4ml.FoldOperation.MIN)
 prov4ml.register_final_metric("MSE_train", 10, prov4ml.FoldOperation.MIN)
-prov4ml.register_final_metric("emissions_rate", 0, prov4ml.FoldOperation.ADD)
+prov4ml.register_final_metric("emissions_rate", 0.0, prov4ml.FoldOperation.ADD)
 
 class MNISTModel(nn.Module):
     def __init__(self):
@@ -85,4 +85,4 @@ for i, (x, y) in tqdm(enumerate(test_loader)):
 prov4ml.log_model(mnist_model, "mnist_model_final")
 
 # save the provenance graph
-prov4ml.end_run()
+prov4ml.end_run(create_graph=True, create_svg=True, create_provenance_collection=True)